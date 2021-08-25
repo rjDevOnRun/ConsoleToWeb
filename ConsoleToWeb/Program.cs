ad36f99b36f175d0fa422c51489c46e7b0fcccfc
RjDevOnrun <john.roby76@gmail.com>
2021-08-25 12:24:52 +0900

    Add project files.

---
+++ ConsoleToWeb/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace ConsoleToWeb
+{
+    /*
+     * Reference: https://www.c-sharpcorner.com/blogs/each-asp-net-core-web-application-is-a-console-application
+     * Convert a Console Project to Asp.NET Core Web Application
+     * 
+     * 1) change csproject settings
+     * 2) Add Generic and Web host builders with options
+     * 3) Add Startup.cs class to the root and ref it in IHostBuilders
+     * 4) Call the Create Host Builder in Main method
+     * 5) Setup the Startup class for register services, routes and middlewares
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        /// <summary>
+        /// Add default Generic Host Builder which enables support 
+        /// for Dependency Injects, Logging, IConfiguration 
+        /// and IHostedServices implementations
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            // Note: You cannot rename CreateHostBuilder method
+            // as this exact name is required 
+
+
+            // Add default generic Host builder otpions
+            // Support for Dependency Injects, Logging,
+            // IConfiguration and IHostedServices implementations
+            return Host.CreateDefaultBuilder(args)
+            
+                // Add Web Host builder options
+                // Support for HTTP, IIS and Kestrol Server as default web-server.
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>(); // ref the Startup class
+                });
+        }
+
+
+    }
+}
---
+++ ConsoleToWeb/Startup.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleToWeb
+{
+    public class Startup
+    {
+
+        /// <summary>
+        /// Register services using DI containers
+        /// </summary>
+        /// <param name="services"></param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //// This is implementaion for Asp.Net Core MVC application
+            services.AddControllersWithViews();
+
+            //// This is implementaion for  Asp.Net Core WebAPI application
+            // services.AddControllers();
+
+            //// This is implementaion for Asp.NET Core Razor application
+            //services.AddRazorPages();
+        }
+
+        /// <summary>
+        /// Setup Http request pipeline to insert the middlewares
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            // Add Routing and Endpoint ---------------------------------------
+            // Routing is basically a mapping between
+            // incoming http request and the resource.
+            app.UseRouting();
+            
+            app.UseEndpoints(endpoints =>
+            {
+
+                //// This is implementaion for Asp.Net Core MVC application
+                endpoints.MapDefaultControllerRoute();
+
+                //// This is implementaion for Asp.NET Core WebAPI
+                //endpoint.MapControllers();
+
+                //// This is implementaion for Asp.Net Core Razor application
+                //endpoints.MapRazorPages();
+
+                // This is default implementations
+                // Add as many routes as required for your application
+                //    endpoints.MapGet("/", async context =>
+                //    {
+                //        await context.Response.WriteAsync("Hello from new web Application");
+                //    });
+
+                //    endpoints.MapGet("/test", async context =>
+                //    {
+                //        await context.Response.WriteAsync("Hello from test endpoint");
+                //    });
+            });
+
+            // ----------------------------------------------------------------
+        }
+
+    }
+}
